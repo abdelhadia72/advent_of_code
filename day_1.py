fb913601528980e1ded001f9985901161c2e2fd5
abdelhadia72 <thisisavnb@gmail.com>
2023-12-01 10:23:34 +0100

    sloved part 1

--- day_1.py
+++ day_1.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python3
 
-with open("input.txt", "r") as f:
-    for i in f:
-        total = []
-        for j in i:
-            if(j.isdigit()):
-                total.append(j)
-        print("".join(total))
+def get_value(line):
+    '''
+    This function returns only the
+    fist and last number after filtering
+    the line from all characters
+    
+    example:
+    line = one5six913lbrcc
+    return -> 53
+    '''
+    value = []
+    for i in line:
+        if(i.isdigit()):
+            value.append(i)
+    return f"{value[0]}{value[-1]}"
+
+def sumit(file):
+    '''
+    This function reads a file and
+    filter numbers from each line by
+    @get_value and sum all returned
+    values into one result
+    '''
+    with open(file, "r") as f:
+        numbers = []
+        for i in f:
+            numbers.append(get_value(i))
+    total = 0
+    for num in numbers:
+        total += int(num)
+    return (total)
+
+
+# print the value 
+print(sumit("input.txt"))
